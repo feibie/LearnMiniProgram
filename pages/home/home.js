665f599adaebbcaaab3ae25b8ea9779d973fc3b8
yyzs <bjf_1@126.com>
2020-05-26 09:47:43 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,81 @@
 // pages/home/home.js
+
+// getApp()获取App()产生的示例对象
+const app = getApp()
+const name = app.globalData.name
+const age = app.globalData.age
+console.log(name)
 Page({
+  // 监听页面的生命周期函数
+  onLoad() {
+    console.log("onLoad")
+    const _this = this
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/n3/recommend',
+      // success: (res) => {
+      //   console.log(res)
+      //   const data = res.data.data.list
+      //   this.setData({
+      //     list:data
+      //   })
+      // }
+      success: function(res) {
+            console.log(res)
+        const data = res.data.data.list
+        _this.setData({
+          list:data
+        })
+      }
+    })
+  },
+  onReady() {
+    console.log("onReady")
+  },
+  onShow() {
+    console.log("onShow")
+  },
+  onHide() {
+    console.log("onHide")
+  },
+  onUnload() {
+    console.log("onUnload")
+  },
 
-  /**
-   * 页面的初始数据
-   */
+  // 初始化数据
   data: {
+    name: 'World',
+    age: 18,
+    students: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'kobe-1', age: 40},
+      {id: 112, name: 'kobe-2', age: 50}
+    ],
+    counter: 0,
+    list: []
+  },
+  // 监听wxml一些事件
+  handleBtnClick(){
+    // 1.界面不会刷新
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+  handleGetUserInfo(event) {
+    console.log(event)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //监听其他事件
+  //页面滚动
+  onPageScroll(obj){
+    console.log(obj)
   }
 })
